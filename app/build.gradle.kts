5801a91489e745f05ec9934da8bab83cf41633e7
MRSa <mrsa@myad.jp>
2025-08-01 22:24:15 +0900

    使用ライブラリ等を最新に更新。

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -2,18 +2,19 @@ plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
     id("com.google.devtools.ksp")
+    id("org.jetbrains.kotlin.plugin.compose") version "2.2.0"
 }
 
 android {
     namespace = "net.osdn.ja.gokigen.wearos.calendar"
-    compileSdk = 34
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "net.osdn.ja.gokigen.wearos.calendar"
         minSdk = 26
-        targetSdk = 33
-        versionCode = 100109
-        versionName = "1.1.9"
+        targetSdk = 36
+        versionCode = 100110
+        versionName = "1.1.10"
         vectorDrawables {
             useSupportLibrary = true
         }
@@ -40,19 +41,18 @@ android {
         }
     }
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_21
+        targetCompatibility = JavaVersion.VERSION_21
     }
     kotlinOptions {
-        jvmTarget = "17"
+        jvmTarget = "21"
     }
     buildFeatures {
         compose = true
     }
-    composeOptions {
-        //kotlinCompilerExtensionVersion = "1.4.3"
-        kotlinCompilerExtensionVersion = "1.5.3"
-    }
+    //composeOptions {
+    //    kotlinCompilerExtensionVersion = "1.5.3"
+    //}
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
@@ -62,36 +62,32 @@ android {
 
 dependencies {
     implementation("androidx.core:core-splashscreen:1.0.1")
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation("androidx.core:core-ktx:1.16.0")
     implementation("androidx.percentlayout:percentlayout:1.0.0")
     implementation("androidx.legacy:legacy-support-v4:1.0.0")
-    implementation("androidx.recyclerview:recyclerview:1.3.2")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.7.0")
+    implementation("androidx.recyclerview:recyclerview:1.4.0")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.9.2")
 
-    implementation("androidx.activity:activity-compose:1.8.2")
+    implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.wear:wear-tooling-preview:1.0.0")
 
-    //val composeVersion = "2023.08.00"
-    val composeVersion = "2024.02.02"
+    val composeVersion = "2025.07.00"
     implementation(platform("androidx.compose:compose-bom:$composeVersion"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-tooling-preview")
 
-    //val wearComposeVersion = "1.2.1"
-    val wearComposeVersion = "1.3.0"
+    val wearComposeVersion = "1.4.1"
     implementation("androidx.wear.compose:compose-foundation:$wearComposeVersion")
     implementation("androidx.wear.compose:compose-material:$wearComposeVersion")
 
-    //val tilesVersion = "1.2.0"
-    val tilesVersion = "1.3.0"
+    val tilesVersion = "1.5.0"
     implementation("androidx.wear.tiles:tiles:$tilesVersion")
     implementation("androidx.wear.tiles:tiles-material:$tilesVersion")
 
-    val horologistVersion = "0.5.3"
-    implementation("com.google.android.horologist:horologist-compose-tools:$horologistVersion")
-    implementation("com.google.android.horologist:horologist-tiles:$horologistVersion")
+    implementation("com.google.android.horologist:horologist-compose-tools:0.6.23")
+    implementation("com.google.android.horologist:horologist-tiles:0.7.15")
 
-    val roomVersion = "2.6.1"
+    val roomVersion = "2.7.2"
     implementation("androidx.room:room-runtime:$roomVersion")
     annotationProcessor("androidx.room:room-compiler:$roomVersion")
     ksp("androidx.room:room-compiler:$roomVersion")
